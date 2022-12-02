08ac572c0a83e13d46f22975295ae9d88668444b
Oscar Olsson <69190482+oscarolsson94@users.noreply.github.com>
2022-12-02 08:41:51 +0100

    Create aoc2b.js

---
+++ aoc2b.js
@@ -0,0 +1,67 @@
+const battles = RAW_INPUT.split(/\r?\n/);
+
+let totalScore = 0;
+
+const checkWinner = (opponentsValue, playerValue) => {
+  if (opponentsValue === playerValue) return "draw";
+
+  if (
+    (opponentsValue === "A" && playerValue === "B") ||
+    (opponentsValue === "B" && playerValue === "C") ||
+    (opponentsValue === "C" && playerValue === "A")
+  )
+    return "win";
+
+  return "lost";
+};
+
+const getChoiceValue = (choice) => {
+  if (choice === "A") return 1;
+  if (choice === "B") return 2;
+  return 3;
+};
+
+const getSupposedOutcomeValue = (manualValue) => {
+  if (manualValue === "Z") return 6;
+  if (manualValue === "Y") return 3;
+  return null;
+};
+
+const getPlayerValue = (outcomeValue, opponentsValue) => {
+  if (outcomeValue === 3) return opponentsValue;
+
+  if (!outcomeValue) {
+    if (opponentsValue === "A") return "C";
+    if (opponentsValue === "B") return "A";
+    if (opponentsValue === "C") return "B";
+  }
+
+  if (outcomeValue === 6) {
+    if (opponentsValue === "A") return "B";
+    if (opponentsValue === "B") return "C";
+    if (opponentsValue === "C") return "A";
+  }
+};
+
+battles.forEach((battle) => {
+  const manualValue = battle[2];
+  const opponentsValue = battle[0];
+
+  const outcomeValue = getSupposedOutcomeValue(manualValue);
+
+  const playerValue = getPlayerValue(outcomeValue, opponentsValue);
+
+  const choiceValue = getChoiceValue(playerValue);
+
+  let roundScore = 0;
+
+  roundScore += choiceValue;
+
+  if (outcomeValue) {
+    roundScore += outcomeValue;
+  }
+
+  totalScore += roundScore;
+});
+
+console.log(totalScore);
